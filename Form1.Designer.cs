51b61153d3312788016e437d120843e52720b8a3
Sebastian <85984736+semazurek@users.noreply.github.com>
2022-03-06 00:12:40 +0100

    Release v.1.5

---
+++ Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(281, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(281, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.richTextBox1.DetectUrls = false;
+            this.richTextBox1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox1.HideSelection = false;
+            this.richTextBox1.Location = new System.Drawing.Point(33, 80);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(495, 120);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.TabStop = false;
+            this.richTextBox1.Text = "Loading...";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Purple;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(0, 259);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(281, 57);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Scan";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(16, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 22);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Selection:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(562, 316);
+            this.panel1.TabIndex = 6;
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.panel2.Controls.Add(this.comboBox2);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(562, 43);
+            this.panel2.TabIndex = 4;
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseMove);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Enabled = false;
+            this.comboBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(467, 6);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(44, 28);
+            this.comboBox2.TabIndex = 0;
+            this.comboBox2.TabStop = false;
+            this.comboBox2.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button3.Location = new System.Drawing.Point(519, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(37, 32);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "X";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button3_MouseClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label5.Location = new System.Drawing.Point(14, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(279, 23);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Driver Download Utility v.1.5";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(28, 57);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(6, 3, 3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(506, 149);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Drivers detected to install";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(122, 219);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(427, 29);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
+            this.comboBox1.MouseEnter += new System.EventHandler(this.comboBox1_MouseEnter);
+            this.comboBox1.MouseHover += new System.EventHandler(this.comboBox1_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(562, 316);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Driver Download Utillity";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace WinFormsApp2
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string line, lineall, entryconverted, entry;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process command = new Process();
+            command.StartInfo.FileName = "powershell.exe";
+            command.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            command.StartInfo.UseShellExecute = false;
+            command.StartInfo.RedirectStandardOutput = true;
+            command.StartInfo.CreateNoWindow = true;
+            command.StartInfo.Arguments = "Get-PnpDevice -Status ERROR | select FriendlyName -ExpandProperty Name | ft -hide";
+            command.Start();
+
+            //save Device Name into string -> show list
+            while (!command.StandardOutput.EndOfStream)
+            {
+                lineall = command.StandardOutput.ReadToEnd();
+                richTextBox1.Text = lineall;
+            }
+
+            //Save log files to put them into comboboxe's 
+            command.StartInfo.Arguments = "Get-PnpDevice -Status ERROR | select FriendlyName -ExpandProperty Name | ft -hide > name.log";
+            command.Start();
+
+            command.StartInfo.Arguments = "Get-PnpDevice -Status ERROR | select InstanceId | findstr VEN_ > id.log";
+            command.Start();
+
+            //save Device Instance ID into String -> button Download
+            command.StartInfo.Arguments = "Get-PnpDevice -Status ERROR | select InstanceId | findstr VEN_";
+            command.Start();
+            while (!command.StandardOutput.EndOfStream)
+            {
+                line = command.StandardOutput.ReadLine();
+            }
+
+            if (line == " " || line == "")
+            {
+                MessageBox.Show("No drivers required to be installed were found.","Information");
+                richTextBox1.Text = "Nothing to do here.";
+
+                command.StartInfo.Arguments = "rm id.log; rm name.log";
+                command.Start();
+
+            }
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            comboBox2.SelectedIndex = comboBox1.SelectedIndex;
+            entry = comboBox2.GetItemText(comboBox2.SelectedItem);
+        }
+
+        private void comboBox1_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_MouseClick(object sender, MouseEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //Panel = Mouse Drag (Flat GUI)
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void comboBox1_MouseEnter(object sender, EventArgs e)
+        {
+            StreamReader sr = File.OpenText("name.log");
+            string s = "";
+            comboBox1.Items.Clear();
+            while ((s = sr.ReadLine()) != null)
+            {
+                comboBox1.Items.Add(s);
+            }
+            sr.Close();
+
+            StreamReader sr2 = File.OpenText("id.log");
+            s = "";
+            comboBox2.Items.Clear();
+            while ((s = sr2.ReadLine()) != null)
+            {
+                comboBox2.Items.Add(s);
+            }
+            sr2.Close();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+
+            button1.BackColor = Color.FromArgb(87, 101, 116);
+            button2.BackColor = Color.FromArgb(44, 62, 80);
+
+            Process command = new Process();
+            command.StartInfo.FileName = "powershell.exe";
+            command.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            command.StartInfo.UseShellExecute = false;
+            command.StartInfo.RedirectStandardOutput = true;
+            command.StartInfo.CreateNoWindow = true;
+            command.StartInfo.Arguments = "Get-PnpDevice -Status ERROR | select FriendlyName -ExpandProperty Name | ft -hide";
+            command.Start();
+
+            //save Device Name into string -> show list
+            while (!command.StandardOutput.EndOfStream)
+            {
+                lineall = command.StandardOutput.ReadToEnd();
+                richTextBox1.Text = lineall;
+            }
+
+            //Save log files to put them into comboboxe's 
+            command.StartInfo.Arguments = "Get-PnpDevice -Status ERROR | select FriendlyName -ExpandProperty Name | ft -hide > name.log";
+            command.Start();
+
+            command.StartInfo.Arguments = "Get-PnpDevice -Status ERROR | select InstanceId | findstr VEN_ > id.log";
+            command.Start();
+
+            //save Device Instance ID into String -> button Download
+            command.StartInfo.Arguments = "Get-PnpDevice -Status ERROR | select InstanceId | findstr VEN_";
+            command.Start();
+            while (!command.StandardOutput.EndOfStream)
+            {
+                line = command.StandardOutput.ReadLine();
+            }
+
+            if (line == " " || line == "")
+            {
+                MessageBox.Show("No drivers required to be installed were found.", "Information");
+                richTextBox1.Text = "Nothing to do here.";
+
+                command.StartInfo.Arguments = "rm id.log; rm name.log";
+                command.Start();
+
+                Application.Exit();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex > -1)
+            {
+                //delete all before VEN_
+                int pos = entry.IndexOf("VEN_");
+                if (pos >= 0)
+                {
+                    entryconverted = entry.Remove(0, pos);
+                }
+
+                //delete all after &SUBSYS
+                pos = entryconverted.IndexOf("&SUBSYS");
+                if (pos >= 0)
+                {
+                    entry = entryconverted.Remove(pos);
+                }
+
+                //replace & symbol between VEN_ and DEV_ ids for URL entry
+                entryconverted = entry.Replace("&", "%26");
+
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = "https://download-drivers.net/search?q=" + entryconverted,
+                    UseShellExecute = true
+                });
+            }
+            else
+            {
+                MessageBox.Show("Choose the driver you want to download.", "Information");
+            }
+        }
+
+    }
+}
